8357c21524d9ceaa9010a7a37e3746ec36ff93dd
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2025-12-03 23:19:51 +0100

    fix power shell script for client

--- zad1-2/client/run.ps1
+++ zad1-2/client/run.ps1
@@ -2,4 +2,4 @@
 
 docker rm -f z32-client-c 2>$null
 docker build -t z32-client-docker .
-docker run -it --network z32_network --name z32-client-c z32-client-docker
+docker run -it --cap-add=NET_ADMIN --network z32_network --name z32-client-c z32-client-docker
